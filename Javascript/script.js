d870816a5ed8b317e7020c445306f903b9dafcdd
Gustavo Almeida <profissional.gustavo.004@gmail.com>
2023-05-29 01:46:23 -0300

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-icon');
+let barranav = document.querySelector('.barranav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    barranav.classList.toggle('abre');
+}
